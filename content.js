1992af5df7eb7233b59569025f5e938a3c089e38
uselesslyuseful <susan.ruoxuan.zheng@gmail.com>
2025-03-16 02:42:21 -0400

    IDK

--- content.js
+++ content.js
@@ -9,7 +9,7 @@ function addGifToScreen(gifSrc) {
     }
 
     const img = document.createElement("img");
-    img.src = gifSrc;
+    img.src = gif.src;
 
     // Calculate random position & size
     const maxWidth = window.innerWidth - 200; // Ensure the GIF stays within the screen
